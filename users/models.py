a55dea5073faabc8d877809e68d2c6116051862b
Vadim Semenov <vadimsemenov53@gmail.com>
2026-08-13 12:14:49 +0300

    add Temporary model User & good test coonection DataBase (launch project)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """ Модель для представления пользователей сервиса. """
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Введите адрес эл.почты")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
