9998a2d6ecf81d7f08c58e38444dbaba3767941f
Vadim Semenov <vadimsemenov53@gmail.com>
2026-08-14 15:18:31 +0300

    pagination add & add ViewSet -> HabitViewSet

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("habits/", include("habits.urls", namespace="habits")),
+    path("habits/", include("habits.urls", namespace="habits")),
     path("users/", include("users.urls", namespace="users")),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.viewsets import ModelViewSet
+from habits.models import Habit
+from habits.paginations import CustomPagination
 
-# Create your views here.
+
+class HabitViewSet(ModelViewSet):
+    """Контроллер для модели Habit использующий ModelViewSet"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        """ Метод переопределения получения данных. """
+        return Habit.objects.filter(
+            Q(user=self.request.user) | Q(is_public=True)
+        )
+
+    def perform_create(self, serializer):
+        """Метод отвечающий за автоматическое заполнение владельца"""
+        habit = serializer.save()
+        habit.user = self.request.user
+        habit.save()
