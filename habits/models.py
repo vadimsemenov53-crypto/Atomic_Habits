ab283d1c3eeb899a400a37552df5a479c52e0145
Vadim Semenov <vadimsemenov53@gmail.com>
2026-08-14 15:01:40 +0300

    add Model -> Habit

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,70 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from config.settings import AUTH_USER_MODEL
 
-# Create your models here.
+
+class Habit(models.Model):
+    """ Класс представления модели 'Привычка'. """
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Создал",
+        help_text="Укажите создателя",
+    )
+    place = models.CharField(
+        max_length=150,
+        verbose_name="Место",
+        help_text="Укажите место выполнения привычки",
+    )
+    time = models.TimeField(
+        verbose_name="Время",
+        help_text="Укажите время выполнения",
+    )
+    action = models.CharField(
+        max_length=250,
+        verbose_name="Действие",
+        help_text="Укажите действие для выполнения",
+    )
+    is_pleasant = models.BooleanField(
+        default=False,
+        verbose_name="Приятная",
+        help_text="Укажите наличие приятно привычки",
+    )
+    related_habit = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="dependent_habits",
+    )
+    period = models.PositiveIntegerField(
+        default=1,
+        validators=[MinValueValidator(1)],
+        verbose_name="Периодичность",
+        help_text="Укажите периодичность выполнения",
+    )
+    reward = models.CharField(
+        max_length=250,
+        verbose_name="Награда",
+        help_text="Укажите награду за выполнение",
+        null=True,
+        blank=True,
+    )
+    duration = models.PositiveIntegerField(
+        verbose_name="Время на выполнение",
+        help_text="Укажите время выполнения привычки."
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name="Публичная",
+        help_text="Укажите публичность",
+    )
+
+    def __str__(self):
+        """ Метод строкового отображения. """
+        return f'я буду {self.action} в {self.time} в {self.place}'
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+        ordering = ("place",)
